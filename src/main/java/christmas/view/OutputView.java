100470e68b6f9a2521eb3eb067a9f86071c1088f
Kim JunHyun <jjag1015@naver.com>
2023-11-11 16:58:07 +0900

    feat(OutputView): OutputView 클래스 생성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,13 @@
+package christmas.view;
+
+public class OutputView {
+
+    public void printStartingMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+
+}
