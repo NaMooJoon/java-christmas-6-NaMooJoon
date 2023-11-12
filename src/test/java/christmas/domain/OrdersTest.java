18a05b11301fb83a0d2a39df7cf0f48b6bb11441
Kim JunHyun <jjag1015@naver.com>
2023-11-12 21:05:24 +0900

    test(Orders): 메뉴 주문 입력시 발생할 수 있는 예외 처리 테스트

---
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrdersTest {
+    @DisplayName("중복된 메뉴를 입력하는 경우, 예외 처리 한다.")
+    @Test
+    void 중복_메뉴_예외_처리() {
+        List<String> names = Arrays.asList("티본스테이크", "바비큐립", "초코케이크", "티본스테이크");
+        List<Order> orders = names.stream()
+                .map(name -> Order.nameAndCountOf(name, 1))
+                .collect(Collectors.toList());
+        assertThatThrownBy(() -> Orders.from(orders))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
