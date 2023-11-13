df50d80545547fef2f2ce50ef3ec7fb1521597cb
Kim JunHyun <jjag1015@naver.com>
2023-11-14 04:10:34 +0900

    feat(EventCalculator): 이벤트 계산 기능 추가

---
+++ src/main/java/christmas/domain/EventCalculator.java
@@ -0,0 +1,72 @@
+package christmas.domain;
+
+import static christmas.domain.constants.Menu.CHAMPAGNE;
+
+import christmas.domain.constants.Badge;
+import christmas.domain.constants.BenefitType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventCalculator {
+    private static final int GIFT_EVENT_MINIMUM_AMOUNT = 120_000;
+    private Date date;
+    private Orders orders;
+
+    private EventCalculator(Date date, Orders orders) {
+        this.date = date;
+        this.orders = orders;
+    }
+
+    public static EventCalculator of(Date date, Orders orders) {
+        return new EventCalculator(date, orders);
+    }
+
+
+    public Order getEventGift() {
+        if (canGetGift()) {
+            return Order.menuAndCountOf(CHAMPAGNE, 1);
+        }
+        return null;
+    }
+
+    public int getExpectedAmount() {
+        return orders.getTotalAmount() - getTotalDiscounts().getTotalAmount();
+    }
+
+    public Badge getBadge() {
+        int amountOfDiscount = getAllBenefits().getTotalAmount();
+        return Badge.getBadgeBy(amountOfDiscount);
+    }
+
+    public Benefits getAllBenefits() {
+        if (orders.getTotalAmount() < 10_000) {
+            return Benefits.from(new ArrayList<>());
+        }
+        Benefits benefits = getTotalDiscounts();
+        if (canGetGift()) {
+            benefits.add(Benefit.of(BenefitType.EVENT_GIFT, getEventGift().getPrice()));
+        }
+        return benefits;
+    }
+
+    private Benefits getTotalDiscounts() {
+        List<Benefit> benefits = new ArrayList<>();
+        if (date.isChristmas()) {
+            benefits.add(Benefit.newChristmas(date));
+        }
+        if (date.isWeekday()) {
+            benefits.add(Benefit.newWeekday(orders.getDesertCount()));
+        }
+        if (date.isWeekend()) {
+            benefits.add(Benefit.newWeekend(orders.getMainCourseCount()));
+        }
+        if (date.isSpecialDay()) {
+            benefits.add(Benefit.newSpecialDay());
+        }
+        return Benefits.from(benefits);
+    }
+
+    private boolean canGetGift() {
+        return orders.getTotalAmount() >= GIFT_EVENT_MINIMUM_AMOUNT;
+    }
+}
---
+++ src/test/java/christmas/domain/EventCalculatorTest.java
@@ -0,0 +1,53 @@
+package christmas.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventCalculatorTest {
+    EventCalculator eventCalculator;
+    @BeforeEach
+    void setUp() {
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(Order.nameAndCountOf("티본스테이크", 1));
+        orderList.add(Order.nameAndCountOf("바비큐립", 1));
+        orderList.add(Order.nameAndCountOf("초코케이크", 2));
+        orderList.add(Order.nameAndCountOf("제로콜라", 1));
+
+        eventCalculator = EventCalculator.of(Date.from(3), Orders.from(orderList));
+    }
+
+    @DisplayName("증정 메뉴를 확인한다.")
+    @Test
+    void 증정_메뉴_확인() {
+        Order gift = eventCalculator.getEventGift();
+        assertThat(gift.toString())
+                .isEqualTo("샴페인 1개");
+    }
+
+    @DisplayName("혜택 내역을 확인한다.")
+    @Test
+    void 혜택_내역_확인() {
+        Benefits benefits = eventCalculator.getAllBenefits();
+        assertThat(benefits.toString())
+                .isEqualTo("크리스마스 디데이 할인: -1,200원\n"
+                        + "평일 할인: -4,046원\n"
+                        + "특별 할인: -1,000원\n"
+                        + "증정 이벤트: -25,000원");
+    }
+
+    @DisplayName("총혜택 금액을 확인한다.")
+    @Test
+    void 총혜택_금액_확인() {
+        Benefits benefits = eventCalculator.getAllBenefits();
+        int total = benefits.getTotalAmount();
+
+        assertThat(total)
+                .isEqualTo(31_246);
+    }
+
+}
